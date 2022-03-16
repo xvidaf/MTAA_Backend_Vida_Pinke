154aca40a3b14c3ce6268b157137731a99e33a80
Liqkerin <vida.filip@gmail.com>
2022-03-16 13:04:24 +0100

    Implemented login endpoint, changed the register POST parameter names, changed base URL.

--- MTAA_Backend_VP/urls.py
+++ MTAA_Backend_VP/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('techsupport/', include('techsupport.urls')),
+    path('', include('techsupport.urls')),
 ]
--- techsupport/views.py
+++ techsupport/views.py
@@ -13,12 +13,12 @@ def register(request):
         j = json.loads(request.body)
 
         try:
-            mail = j['mail']
+            mail = j['email']
         except:
             mail = False
 
         try:
-            pwd = j['pass']
+            pwd = j['password']
         except:
             pwd = False
 
@@ -26,6 +26,31 @@ def register(request):
             try:
                 u = User(email=mail, password=pwd, usertype="user", lastlogin=last_login)
                 u.save()
+                return HttpResponse(200)
+            except:
+                return HttpResponse(400)
+        else:
+            return HttpResponse(400)
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        j = json.loads(request.body)
+        try:
+            mail = j['email']
+        except:
+            mail = False
+
+        try:
+            pwd = j['password']
+        except:
+            pwd = False
+
+        if mail and pwd:
+            try:
+                authenticated = User.objects.get(email=mail, password=pwd)
+                authenticated.lastlogin = datetime.now()
+                authenticated.save()
                 return HttpResponse(200)
             except:
                 return HttpResponse(400)
